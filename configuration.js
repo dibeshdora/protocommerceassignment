a3a0ee55a14f5e25bdb7bcc5c474e05dc1e2a73f
dibeshdora <63849898+dibeshdora@users.noreply.github.com>
2020-04-22 17:14:51 +0530

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -2,7 +2,7 @@ var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['ShoppingPage.js'],
+  specs: ['RegistrationPage','ShoppingPage.js'],
 
 onPrepare: function(){
 	
